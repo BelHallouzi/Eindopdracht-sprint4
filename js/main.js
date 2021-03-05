74d3bafda153cafa873c99d28e7d3dac0d942d29
Bilal el Hallouzi <302562632@student.rocmondriaan.nl>
2021-03-05 11:40:10 +0100

    Sprint 1 gemaakt

--- js/main.js
+++ js/main.js
@@ -1,1 +1,8 @@
 // Dit bestand is nu leeg, het is aan jullie om het te vullen :-D Succes!
+const myName = "Bilal";
+const studentNumber = 302562632;
+
+function alertBox() {
+    alert("Naam: " + myName + "   Studentnummer: " + studentNumber);
+}
+alertBox();
